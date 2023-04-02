59b6039406e345d7a5b195b7858066887f2f5b7f
Keeno Jingco <enriquejingco@gmail.com>
2023-04-02 22:02:26 +0100

    adding controller return types

--- app/Http/Controllers/City/IndexController.php
+++ app/Http/Controllers/City/IndexController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\City;
 
 use App\Http\Controllers\Controller;
 use App\Models\City;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class IndexController extends Controller
 {
-    public function __invoke()
+    public function __invoke(): JsonResponse
     {
         $cities = City::all();
 
--- app/Http/Controllers/City/StoreController.php
+++ app/Http/Controllers/City/StoreController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers\City;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\City\StoreRequest;
 use App\Models\City;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class StoreController extends Controller
 {
-    public function __invoke(StoreRequest $request)
+    public function __invoke(StoreRequest $request): JsonResponse
     {
         $city = City::create([
             'name' => $request->get('city')
